bd62e30677e3fa8f61e219c9b52e11bde1d3136e
Jack Williams <jackw@orphans.co.uk>
2022-12-06 09:55:32 +0000

    Static variable

--- index.php
+++ index.php
@@ -690,16 +690,34 @@
 
 // Static Variables 
 
-$x = 5; // This would be a global scope meaning it can be accessed all throughout the script and if included in any other file like 'include(example.php)'
+// $x = 5; // This would be a global scope meaning it can be accessed all throughout the script and if included in any other file like 'include(example.php)'
 
 // Variables in a function have local scope so they must be defined within the function
 // or you can define it as a perameter and an argument
 // or you can use the global keyword to access the variable globally
 
-        function foo() {
-            global $x; //globally referencing the $x variable so the function can access it
-            $x = 10; // you can also change the variable within the function and it will apply to the original variable when the function is called
-            echo $x;
-        }
-        foo();
+        // function foo() {
+        //     global $x; //globally referencing the $x variable so the function can access it
+        //     $x = 10; // you can also change the variable within the function and it will apply to the original variable when the function is called
+        //     echo $x;
+        // }
+        // foo();
 
+// The static keyword is also used to declare variables in a function which keep their value after the function has ended.
+    function getValue() {
+        static $value = null; // Static variables are called one time after that they can not be called again with the same request
+        if($value === null) {
+            $value = someVeryExpensiveFunction();
+        }
+        return $value;
+        }
+
+    function someVeryExpensiveFunction() {
+        sleep(2);
+        echo 'Processing';
+        return 10;
+    }
+
+    getValue() . '<br />'; // = 'Processing10'
+    getValue() . '<br />'; // = '10'
+    getValue() . '<br />'; // = '10'
